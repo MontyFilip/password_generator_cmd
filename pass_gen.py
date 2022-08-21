11ca93b926bba4a93c4e7ec00934bbeb8a85f8e7
Filip Lysiak <filip.lysiak.t1@gmail.com>
2022-08-21 22:20:51 +0200

    update view

--- pass_gen.py
+++ pass_gen.py
@@ -1,9 +1,9 @@
 import random
 
-lower_case='abcdefghijklmnopqrstuwxyz'
-upper_case='ABCDEFGHIJKLMNOPQRSTUWZYZ'
+lower_case = 'abcdefghijklmnopqrstuwxyz'
+upper_case = 'ABCDEFGHIJKLMNOPQRSTUWZYZ'
 number = '0123456789'
-symbols='!@#$%^&*=+/\<>'
+symbols = '!@#$%^&*=+/\<>'
 
 use = lower_case + upper_case + number + symbols
 
@@ -11,4 +11,5 @@ length_pass = 8
 
 password = ''.join(random.sample(use, length_pass))
 
-print("Your generated password is: " + password)
+
+print("\nYour generated password is: " + password)
