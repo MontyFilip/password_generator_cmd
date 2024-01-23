059f66e87864ba32827aa7cc17f15c0412af65b6
Filip Łysiak <95583542+MontyFilip@users.noreply.github.com>
2024-01-23 14:21:00 +0100

    Update pass_gen.py

--- pass_gen.py
+++ pass_gen.py
@@ -1,18 +1,26 @@
 import random
 
-lower_case = 'abcdefghijklmnopqrstuwxyz'
-upper_case = 'ABCDEFGHIJKLMNOPQRSTUWZYZ'
-number = '0123456789'
-symbols = '!@#$%^&*+/\<>'
+def generate_password():
+    lower_case = 'abcdefghijklmnopqrstuwxyz'
+    upper_case = 'ABCDEFGHIJKLMNOPQRSTUWZYZ'
+    number = '0123456789'
+    symbols = '!@#$%^&*+/\\<>'
+    
+    use = lower_case + upper_case + number + symbols
 
-use = lower_case + upper_case + number + symbols
+
+    try:
+        length_pass = int(input('Give me a length for the password (minimum 8 characters): '))
+        if length_pass < 8:
+            print('ERROR: Password length must be at least 8 characters.')
+        else:
+            password = ''.join(random.sample(use, length_pass))
+            return password
+    except ValueError:
+        print('ERROR: Invalid length! Please provide a valid number.')
 
 try:
-    length_pass = int(input('Give me a length password: '))
+    print("\nYour generated password is:", generate_password())
 
-    password = ''.join(random.sample(use, length_pass))
-
-    print("\nYour generated password is: " + password)
-
-except ValueError:
-    print('ERROR value lenght !  Please give a number')
+except KeyboardInterrupt:
+    print("\n\nPassword generation interrupted by user.")
